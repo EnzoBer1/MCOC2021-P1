46f767ab30cd7855041f967711448fdb8e08f9c4
Sandra-Munoz <88350644+Sandra-Munoz@users.noreply.github.com>
2021-09-22 18:18:33 -0300

    Add files via upload

--- barra.py
+++ barra.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 19:56:48 2021
+
+@author: sandramunozavila
+"""
 import numpy as np
 #Hola probando
 from constantes import g_, ρ_acero, E_acero
@@ -50,22 +57,54 @@ class Barra(object):
         
     def obtener_rigidez(self, ret):
         
-        """Implementar"""	
+        ni = self.ni
+        nj = self.nj
+
+        xi = ret.xyz[ni,:]
+        xj = ret.xyz[nj,:]
         
-        return 0
+        Lx=(xj[0]-xi[0])
+        Ly=(xj[1]-xi[1])
+        Lz=(xj[2]-xi[2])
+        
+        costhetax=Lx/self.calcular_largo(ret)
+        costhetay=Ly/self.calcular_largo(ret)
+        costhetaz=Lz/self.calcular_largo(ret)
+        
+        T=np.array([-costhetax, -costhetay, -costhetaz, costhetax, costhetay, costhetax])
+        
+        Ke=self.seccion.area()*E_acero/self.calcular_largo(ret)*T.T@T
+        
+        return Ke
 
     def obtener_vector_de_cargas(self, ret):
         
-        """Implementar"""	
-        
-        return 0
+        return (self.calcular_peso(ret))/2*np.array([0,1,0,1])
 
 
     def obtener_fuerza(self, ret):
         
-        """Implementar"""	
+        ni = self.ni
+        nj = self.nj
+
+        xi = ret.xyz[ni,:]
+        xj = ret.xyz[nj,:]
         
-        return 0
+        Lx=(xj[0]-xi[0])
+        Ly=(xj[1]-xi[1])
+        Lz=(xj[2]-xi[2])
+        
+        costhetax=Lx/self.calcular_largo(ret)
+        costhetay=Ly/self.calcular_largo(ret)
+        costhetaz=Lz/self.calcular_largo(ret)
+        
+        T=np.array([-costhetax, -costhetay, -costhetaz, costhetax, costhetay, costhetax])
+        
+        u_e=np.array([ret.u[3*ni], ret.u[3*ni+1], ret.u[3*ni+2], ret.u[3*nj], ret.u[3*nj+1], ret.u[3*nj+2]])
+        
+        se=self.seccion.area()*E_acero/self.calcular_largo(ret)*T*u_e
+        
+        return se
 
 
 
@@ -92,5 +131,3 @@ class Barra(object):
         """Implementar"""	
         
         return 0
-
-
